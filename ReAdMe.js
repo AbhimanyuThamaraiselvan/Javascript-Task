1ffc12548f04eb9f3c0e659cb86e7583bcab15bf
AbhimanyuThamaraiselvan <itstamaraimani@gmail.com>
2024-09-25 10:59:16 +0530

    firts msg

---
+++ ReAdMe.js
@@ -0,0 +1,18 @@
+/* 1. Open terminal in command promt
+2. dir command - list all files and folders in current directory
+3. cd command - change directory
+4. cd .. - go back to parent directory
+5. mkdir - create new directory
+6. cd folder name - go to folder name directory
+7. code . - open in vscode
+8. git -v or git --version - check the version of git
+9.  Git init - initialize repository
+10. open github.com and create a new repository 
+-give the repository name
+-leave the visibility as public
+-create repository
+
+11. copy the url of repository.
+12. git config --global user.name "  "
+13. git config --global user.email "  "
+*/
